143b6c2d90a64cb0159423736cc2933223ac58d7
Swarup Dey <sdey200598@gmail.com>
2026-06-25 12:28:32 +0530

    Modified Logs

--- index.js
+++ index.js
@@ -1,2 +1,2 @@
-console.log("Hello Devops! Welcome to the world of automation and continuous integration.");
+console.log("Hello Devops!! Welcome to the world of automation and continuous integration.");
 console.log("Stay tuned for more updates and features in this application.");
